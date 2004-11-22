f3e98a954db53ff59253cf63606965b8b685a7e4
Jim Fulton <jim@zope.com>
2004-11-22 10:11:12 +0000

    Moved the definition of the interface IHeaderOutput from zope.server.interfaces to zope.publisher.interfaces.http. It isn't specific to the Zope server. It is needed, for example, by wsgi and by testing servers.

--- interfaces/http.py
+++ interfaces/http.py
@@ -225,6 +225,42 @@ class IHTTPApplicationResponse(IApplicationResponse):
         """Causes a redirection without raising an error.
         """
 
+class IHeaderOutput(Interface):
+    """Interface for setting HTTP response headers.
+
+    This allows the HTTP server and the application to both set response
+    headers.
+
+    zope.publisher.http.HTTPResponse is optionally passed an
+    object which implements this interface in order to intermingle
+    its headers with the HTTP server's response headers,
+    and for the purpose of better logging.
+    """
+
+    def setResponseStatus(status, reason):
+        """Sets the status code and the accompanying message.
+        """
+
+    def setResponseHeaders(mapping):
+        """Sets headers.  The headers must be Correctly-Cased.
+        """
+
+    def appendResponseHeaders(lst):
+        """Sets headers that can potentially repeat.
+
+        Takes a list of strings.
+        """
+
+    def wroteResponseHeader():
+        """Returns a flag indicating whether the response
+
+        header has already been sent.
+        """
+
+    def setAuthUserName(name):
+        """Sets the name of the authenticated user so the name can be logged.
+        """
+
 
 class IHTTPResponse(IResponse):
     """An object representation of an HTTP response.
