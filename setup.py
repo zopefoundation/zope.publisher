68200c2d1de314cd76ae1e82d6f8b24d23040442
Baiju Muthukadan <baiju.m.mail@gmail.com>
2007-03-21 05:53:10 +0000

    - Increment version to 3.4dev  - Set svn properties

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import os
 from setuptools import setup, find_packages
 
 setup(name='zope.publisher',
-      version='3.3dev',
+      version='3.4dev',
       url='http://svn.zope.org/zope.publisher',
       license='ZPL 2.1',
       description='Zope publisher',
