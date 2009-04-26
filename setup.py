b3a7507b2759904af5d18c21b20a25573f2b8a89
Hanno Schlichting <hanno@hannosch.eu>
2009-04-26 14:33:13 +0000

    Update version after release

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ sample = zope.publisher.tests.test_paste:SamplePublication
 """
 
 setup(name='zope.publisher',
-      version='3.6.4',
+      version='3.6.5dev',
       url='http://pypi.python.org/pypi/zope.publisher',
       license='ZPL 2.1',
       author='Zope Corporation and Contributors',
