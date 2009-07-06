dc643719b55ac0fa21f65a1560a3386f2d217563
Uli Fouquet <uli@gnufix.de>
2009-07-06 15:25:29 +0000

    Avoid unnecessary assigns.

--- src/zope/publisher/publish.py
+++ src/zope/publisher/publish.py
@@ -143,9 +143,9 @@ def publish(request, handle_errors=True):
                                 obj, request, exc_info, True)
 
                             if not handle_errors:
-                                dummy, exc_value, dummy = exc_info
                                 reraise = component.queryAdapter(
-                                    exc_value, IReRaiseException, default=None)
+                                    exc_info[1], IReRaiseException,
+                                    default=None)
                                 if reraise is None or reraise():
                                     raise
                     finally:
