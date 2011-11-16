05c2c094ba4c79687efae67e4ec1ec22534c1b21
Wolfgang Schnerring <wosc@wosc.de>
2011-11-16 14:29:26 +0000

    Add test convenience helper ``create_interaction`` and ``with interaction()``.

---
+++ src/zope/publisher/testing.py
@@ -0,0 +1,41 @@
+#############################################################################
+#
+# Copyright (c) 2011 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import contextlib
+import zope.publisher.browser
+import zope.security.management
+import zope.security.testing
+
+
+# The interaction helpers conceptually rather belong to zope.security, but
+# since that doesn't (and shouldn't) depend on zope.publisher, we have to put
+# them here, unfortunately.
+
+def create_interaction(principal_id, **kw):
+    principal = zope.security.testing.Principal(principal_id, **kw)
+    request = zope.publisher.browser.TestRequest()
+    request.setPrincipal(principal)
+    zope.security.management.newInteraction(request)
+    return principal
+
+
+@contextlib.contextmanager
+def interaction(principal_id, **kw):
+    if zope.security.management.queryInteraction():
+        # There already is an interaction. Great. Leave it alone.
+        yield
+    else:
+        principal = create_interaction(principal_id)
+        yield principal
+        zope.security.management.endInteraction()
