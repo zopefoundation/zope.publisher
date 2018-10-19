0cbf451433ac86e433390e319ff0ce5c33f3f556
Jason Madden <jamadden@gmail.com>
2018-10-19 09:17:32 -0500

    Back to development: 5.0.2

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ tests_require = [
 
 setup(
     name='zope.publisher',
-    version='5.0.1',
+    version='5.0.2.dev0',
     url='https://github.com/zopefoundation/zope.publisher',
     license='ZPL 2.1',
     author='Zope Foundation and Contributors',
