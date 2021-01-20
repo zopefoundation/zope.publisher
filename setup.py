a73dbfed4f2f7d545c7d9cd4833399acfc3e080a
Michael Howitz <mh@gocept.com>
2021-01-20 08:08:18 +0100

    Back to development: 6.0.1

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ tests_require = [
 
 setup(
     name='zope.publisher',
-    version='6.0.0',
+    version='6.0.1.dev0',
     url='https://github.com/zopefoundation/zope.publisher',
     license='ZPL 2.1',
     author='Zope Foundation and Contributors',
