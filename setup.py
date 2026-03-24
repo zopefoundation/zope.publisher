84ff3c06a075451585cd4a96a8d36f7479cb243f
Michael Howitz <icemac@gmx.net>
2026-03-24 09:05:57 +0100

    Move package metadata from setup.py to pyproject.toml.

--- setup.py
+++ setup.py
@@ -16,81 +16,8 @@
 # When developing and releasing this package, please follow the documented
 # Zope Toolkit policies as described by this documentation.
 ##############################################################################
-import os
-
 from setuptools import setup
 
 
-def read(*rnames):
-    with open(os.path.join(os.path.dirname(__file__), *rnames)) as f:
-        return f.read()
-
-
-entry_points = {
-    'paste.app_factory': [
-        'main = zope.publisher.paste:Application',
-    ],
-    'zope.publisher.publication_factory': [
-        'sample = zope.publisher.tests.test_paste:SamplePublication',
-    ],
-}
-
-tests_require = [
-    'zope.testing',
-    'zope.testrunner >= 6.4',
-]
-
-setup(
-    name='zope.publisher',
-    version='8.1.dev0',
-    url='https://github.com/zopefoundation/zope.publisher',
-    license='ZPL-2.1',
-    author='Zope Foundation and Contributors',
-    author_email='zope-dev@zope.dev',
-    description='The Zope publisher publishes Python objects on the web.',
-    long_description=read('README.rst') + '\n\n' + read('CHANGES.rst'),
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Zope Public License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
-        'Programming Language :: Python :: 3.13',
-        'Programming Language :: Python :: 3.14',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Topic :: Internet :: WWW/HTTP',
-    ],
-    install_requires=[
-        'multipart>=1.1.0',
-        'setuptools',
-        'zope.browser',
-        'zope.component',
-        'zope.configuration',
-        'zope.contenttype>=4.0.0',
-        'zope.event',
-        'zope.exceptions',
-        'zope.i18n>=4.0.0',
-        'zope.interface>=4.0.1',
-        'zope.location',
-        'zope.proxy',
-        'zope.security>=4.0.0',
-    ],
-    extras_require={
-        'test': tests_require,
-        'docs': [
-            'Sphinx',
-            'repoze.sphinx.autointerface',
-            'sphinx_rtd_theme',
-        ],
-    },
-    entry_points=entry_points,
-    include_package_data=True,
-    zip_safe=False,
-    python_requires='>=3.10',
-)
+# See pyproject.toml for package metadata
+setup()
